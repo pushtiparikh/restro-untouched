b5aa8f55b477443ce9e102c099488b44f426905a
Pushti Parikh <98299460+pushtiparikh@users.noreply.github.com>
2023-12-31 22:36:19 +0530

    Update license.js

--- src/views/global-settings/license.js
+++ src/views/global-settings/license.js
@@ -3,7 +3,7 @@ import { Button, Card, Col, Form, Input, Row } from 'antd';
 import installationService from '../../services/installation';
 
 export default function License({ next }) {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [form] = Form.useForm();
 
   const onFinish = (values) => {
